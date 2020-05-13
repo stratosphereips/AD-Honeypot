correct masking of outputs

dynamic_rnn.py
@@ -23,7 +23,7 @@ class DeepSetAggregationLayer(tf.keras.Model):
             input_array = input_data
         tmp_array = tf.TensorArray(dtype=tf.float32,size=input_array.size())
         input_array = input_data
-        for i in range(input_array.size()):
+        for i in tf.range(input_array.size()):
             tmp_array = tmp_array.write(i,self.inner_fn(input_array.read(i)))
         aggregated = tf.math.reduce_sum(tmp_array.stack(), axis=0)
         return self.outer_fn(aggregated)
@@ -39,34 +39,43 @@ class DynamicRNNEncoder(tf.keras.Model):
     def call(self, input_data, conditions):
         # [batch, time, features] -> [time, batch, features]
         input_data = tf.transpose(input_data, [1, 0, 2])
-        #[batch]
-        conditions = tf.transpose(conditions, [1, 2, 0])
+        #[batch,node,parents] -> [node, parents,batch]
+        conditions = tf.transpose(conditions, [1,2,0])
         #outputs = tf.TensorArray(tf.float32, input_data.shape[0] + 1)
         next_state = self.cell.get_initial_state(input_data[0,:,:])
         outputs = tf.TensorArray(tf.float32, 0,dynamic_size=True, element_shape=next_state.shape)
-        #next_state = self.cell.get_initial_state(input_data[0,:,:])
         if input_data.shape[0] == None:
-            l = 100
+            l = 1
         else:
             l = input_data.shape[0]
-            for i in range(input_data.shape[0]):
-                outputs = outputs.write(i,tf.zeros_like(next_state))
+        idx = 1
         for i in tf.range(l):
             if i > 0:
                 previous_states = outputs.identity()
-                previous_states = previous_states.stack()
-
-                mask = conditions[i]
-                
+                previous_states = previous_states.stack() #[i, rnn_dim, batch]
+                #tf.print("previous_states:", previous_states)
+                #tf.print("-------------------------")
+                 
+                mask = conditions[i,:idx,:] # mask shape [nodes, batch]
+                #tf.print("Mask shape:",mask.shape)
+                idx += 1
                 mask = tf.stack([mask for k in range(previous_states.shape[-1])],axis=2)
+                #tf.print("mask:", mask)
+                #tf.print("-------------------------")
                 #print(mask.shape, previous_states.shape)
                 masked = previous_states*mask
+                #tf.print("masked", masked)
+                #tf.print("--------------")
                 aggregated = tf.math.reduce_sum(masked,axis=0)
+                #tf.print(aggregated)
                 #prepare next 
                 next_state = aggregated
+    
+            #tf.print("####################################")
             output, state = self.cell(input_data[i], next_state)
             #append to the TensorArray
             outputs = outputs.write(i, output)
+            #print("outputs:", outputs.size())
 
         if self.output_aggregator:
             return self.output_aggregator(outputs)
@@ -98,15 +107,14 @@ class DynamicRNNDecoder(tf.keras.Model):
 
         self.N_module = out_mean = MLP([(modul_hidden_size,"relu"),(1,"relu")])
 
-        self.feature_module = MLP([(modul_hidden_size,"relu"),(feature_dim,"softmax")])
-        self.edge_module = MLP([(modul_hidden_size,"relu"),(1,"sigmoid")])
-        self.init_module = MLP([(modul_hidden_size,"relu"),(rnn_dim,"relu")])
+        self.feature_module = MLP([(modul_hidden_size,"relu"),(modul_hidden_size,"relu"),(feature_dim,"softmax")])
+        self.edge_module = MLP([(modul_hidden_size,"relu"),(modul_hidden_size,"relu"),(1,"sigmoid")])
+        self.init_module = MLP([(modul_hidden_size,"relu"),(modul_hidden_size,"relu"),(rnn_dim,"relu")])
         self.graph_aggregator = DeepSetAggregationLayer(tf.keras.layers.Dense(agg_hidden_size,activation="relu"),tf.keras.layers.Dense(z_dim,activation="relu"))
         self.node_aggregator = DeepSetAggregationLayer(tf.keras.layers.Dense(agg_hidden_size,activation="relu"),tf.keras.layers.Dense(self.rnn_cell.units,activation="relu"))
 
     @tf.function
     def call(self, in_encoding):
-
         #in_encoding shape [1,encoding_dim] - [batch_size, embedding_size]
         #target_nodes shape [1,1] - [batch_size, 1]
 
@@ -116,16 +124,17 @@ class DynamicRNNDecoder(tf.keras.Model):
         N = tf.squeeze(tf.random.poisson([1],lambdas, dtype=tf.dtypes.float32))
         N = tf.cast(N,tf.int32)
         self._test = N
-        max_nodes = tf.reduce_max(N)
-        #test = [x for x in N]
+        max_nodes = tf.cast(tf.reduce_max(N),tf.int32)
         if max_nodes <= 0:
-            max_nodes =1
-        batch_size = lambdas.shape[0] if lambdas.shape[0] else 1
+            max_nodes = 1
+
+        batch_size = in_encoding.get_shape().as_list()[0]
+        batch_size = batch_size if batch_size else 1
 
         #self._feature_mask = None
-        h_nodes = tf.TensorArray(dtype=tf.float32,size=max_nodes,clear_after_read = False).unstack(tf.zeros([max_nodes, batch_size, self.rnn_cell.units]))
-        f_nodes = tf.TensorArray(dtype=tf.float32,size=max_nodes,clear_after_read = False).unstack(tf.zeros([max_nodes, batch_size, self.feature_dim]))
-        e_nodes = tf.TensorArray(dtype=tf.float32,size=max_nodes,clear_after_read = False).unstack(tf.zeros([max_nodes, max_nodes, batch_size]))
+        h_nodes = tf.TensorArray(dtype=tf.float32,size=0,clear_after_read = False, dynamic_size=True, element_shape=[batch_size, self.rnn_cell.units])
+        f_nodes = tf.TensorArray(dtype=tf.float32,size=0,clear_after_read = False, dynamic_size=True, element_shape=[batch_size, self.feature_dim])
+        e_nodes = tf.TensorArray(dtype=tf.float32,size=1,clear_after_read = False, dynamic_size=True).unstack(tf.zeros([max_nodes, max_nodes, batch_size]))
 
         #initialize hidden state of first node and graph encoding
         h_nodes = h_nodes.write(0,self.init_module(in_encoding))
@@ -133,41 +142,81 @@ class DynamicRNNDecoder(tf.keras.Model):
 
         #estimate features of the first node
         f_nodes = f_nodes.write(0,self.feature_module(h_nodes.read(0)))
-        
-        
-        add_edges = tf.TensorArray(dtype=tf.float32,size=max_nodes,clear_after_read = False,element_shape=e_nodes.read(0).shape)
-        for i in range(max_nodes):
-            add_adges = add_edges.write(i,tf.zeros_like(e_nodes.read(0)))
+
+    
+        idx = 1
         for i in tf.range(1,max_nodes):
+            #tf.print(f"Processing node {idx}")
+            #create tmporal array for edges to node i
+            add_edges = tf.TensorArray(dtype=tf.float32,size=max_nodes, clear_after_read = False).unstack(tf.zeros([max_nodes, 1, batch_size]))
+            #tf.print("init OK")    
             for j in tf.range(i):
                 #join values of hidden state of Node j([batch,rnn_dim]) and H_graph[batch, encoding_dim] -> [batch,rnn_dim + encoding_dim]
                 merged = tf.concat([H_graph, h_nodes.read(j)],axis=1) 
-                #estimate edges
-                add_adge = self.edge_module(merged) #shape [batch,1]
-                add_edges = add_edges.write(j,tf.transpose(add_adge))
-            e_nodes = e_nodes.write(i,add_edges.concat())
-            #cleanup the temporal array
-            for l in range(max_nodes):
-                add_adges = add_edges.write(l,tf.zeros_like(e_nodes.read(0)))
-
+                #estimate edge from node j to node i
+                add_edge = self.edge_module(merged) #shape [batch,1]
+                #write result in tmp array
+                add_edges = add_edges.write(j,tf.transpose(add_edge))    
+            #covert to binary values based on self.edge_threshold
+            binary = add_edges.concat()
+            #tf.print("RAW_SHAPE:")
+            #tf.print(binary.shape)
+            #tf.print("Raw:")
+            #tf.print(binary)
+            #tf.print("-----------------------------")
+            binary = tf.where(binary >= self.edge_treshold, 1.0, 0.0)
+            #tf.print("binary:")
+            #tf.print(binary)
+            #tf.print("-----------------------------")
+            #write in the edge array
+            e_nodes = e_nodes.write(i, binary)
+            
             #make a copy of hidden states and create tensor
             previous_states = h_nodes.identity()
             previous_states = previous_states.stack()
             
             #aggregate ancestors
-            mask_tmp = e_nodes.read(i) #shape [N,batch]
+            #tf.print("previous:")
+            #tf.print(previous_states)
+            mask_tmp = e_nodes.read(i)[:idx,:] #shape [i:idx, batch]
+            #tf.print("-----------------------------")
+            #tf.print("mask_tmp", mask_tmp)
             mask = tf.stack([mask_tmp for k in range(previous_states.shape[-1])],axis=2)
-            mask = tf.where(mask >= self.edge_treshold,1.0,0.0)
+            #tf.print("-----------------------------")
+            #tf.print("mask:",mask)
+            #tf.print("-----------------------------")
             masked = previous_states*mask
+            #tf.print("masked:", masked)
+            #tf.print("-----------------------------")
             ancestors = tf.math.reduce_sum(masked,axis=0)
+            #tf.print("ancestors:", ancestors)
+            
+
 
             #find sinknodes
             children_view = e_nodes.identity().stack()
-
+            #tf.print("children_view:")
+            #tf.print(children_view)
+            #tf.print("-----------------------------")
+            #tf.print("trimmed:")
+            children_view = children_view[:idx,:idx,:]
+            #tf.print(children_view)
+            #tf.print("-----------------------------")
             has_children_mask = tf.math.reduce_sum(children_view,axis=0)
-            sink_mask = tf.where(has_children_mask < self.edge_treshold, 1.0,0.0)
+            #tf.print("CHW mask:")
+            sink_mask = tf.where(has_children_mask == 0, 1.0,0.0)
+            #tf.print(sink_mask)
+            #tf.print("-----------------------------")
             sink_mask = tf.stack([sink_mask for k in range(previous_states.shape[-1])],axis=2)
+            #tf.print("Extended:")
+            #tf.print(sink_mask)
+            #tf.print("-----------------------------")
+            #tf.print("previous:")
+            #tf.print(previous_states)
+            #tf.print("-----------------------------")
             sink_masked = previous_states*sink_mask
+            #tf.print("masked:")
+            #tf.print(sink_masked)
             sink_nodes = tf.math.reduce_sum(sink_masked,axis=0)
 
             #get current node encoding from RNN cell
@@ -178,5 +227,8 @@ class DynamicRNNDecoder(tf.keras.Model):
             
             #update graph encoding
             H_graph = self.graph_aggregator(h_nodes.identity())
+            idx += 1
+            #tf.print("#####################################")
         
+
         return lambdas,tf.transpose(f_nodes.stack(), [1, 0, 2]), tf.transpose(e_nodes.stack(), [2, 0, 1]), N